a5171c90d9d6c6f3a19fcfb1eb18b88c56a0dcaa
cdeveloper123 <cdeveloper2016@gmail.com>
2025-02-17 21:23:59 +0530

    Update messages details

--- front-end/src/components/user/dashboard/Messages/MessageBookingDetails.jsx
+++ front-end/src/components/user/dashboard/Messages/MessageBookingDetails.jsx
@@ -23,7 +23,7 @@ const MessageBookingDetails = ({ setOpenBooking, openBooking, chatInfo }) => {
 
   return (
     <div
-      className={`transition-all duration-300 max-h-screen xl:w-[610px] 2xl:w-[580px] min-h-full overflow-y-auto scrollbar-hide font-inter ${
+      className={`transition-all duration-300 max-h-screen xl:w-[590px] 2xl:w-[580px] w-[340px] min-h-full overflow-y-auto scrollbar-hide font-inter ${
         openBooking
           ? "z-50 fixed xl:static  top-0 right-0 bg-[#FCFDFC] border border-gray-200 rounded xl:rounded-none pb-4"
           : "hidden xl:block border-l border-gray-300 "
@@ -72,7 +72,7 @@ const MessageBookingDetails = ({ setOpenBooking, openBooking, chatInfo }) => {
             return (
               <div key={index} className="text-lg">
                 <p className="mb-4 text-gray-500">Check in</p>
-                <div className="flex gap-16 xl:gap-[70px] 2xl:gap-[60px] mb-8">
+                <div className="flex gap-16 xl:gap-[60px] 2xl:gap-[60px] mb-8">
                   <div className="flex gap-1 items-center text-nowrap">
                     <p>{item.timeIn.date}</p>{" "}
                     <img
@@ -93,7 +93,7 @@ const MessageBookingDetails = ({ setOpenBooking, openBooking, chatInfo }) => {
                   </div>
                 </div>
                 <p className="mb-4 text-gray-500">Check out</p>
-                <div className="flex gap-16 xl:gap-[70px] 2xl:gap-[60px]">
+                <div className="flex gap-16 xl:gap-[60px] 2xl:gap-[60px]">
                   <div className="flex gap-2 text-nowrap items-center">
                     <p>{item.timeOut.date}</p>{" "}
                     <img
