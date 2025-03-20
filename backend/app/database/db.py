989a3f95f39177e05a688e15ff7d3b51eab40912
cdeveloper123 <cdeveloper2016@gmail.com>
2025-03-20 15:36:37 +0530

    refactor: update database URL configuration for local development

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -3,7 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+# for local development uncomment the following code
+
+from dotenv import load_dotenv 
+load_dotenv()
+SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+
+# SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL') # comment this line for local development
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
